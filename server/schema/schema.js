3f147eaa8d16851dccef6f2fc4c128f3059e96fa
tlc2880 <tommylcao@gmail.com>
2023-08-05 11:11:25 -0700

    Add schema project(id) and projects

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -9,6 +9,23 @@ const {
 } = require('graphql');
 
 // Client Type
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return clients.find((client) => client.id === parent.clientId)
+      }
+    }
+  }),
+});
+
+// Client Type
 const ClientType = new GraphQLObjectType({
   name: 'Client',
   fields: () => ({
@@ -22,6 +39,20 @@ const ClientType = new GraphQLObjectType({
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
+    projects: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, arg) {
+        return projects;
+      }
+    },
+    project: {
+      type: ProjectType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return projects.find(project => project.id === args.id);
+      }
+    },
+ 
     clients: {
       type: new GraphQLList(ClientType),
       resolve(parent, arg) {
