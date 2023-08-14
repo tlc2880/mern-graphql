8dd918fe5e2dbaf948b0ed299d90b2f40a8bfca3
tlc2880 <tommylcao@gmail.com>
2023-08-14 07:13:05 -0700

    Setup client create-react-app

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -97,6 +97,23 @@ const mutation = new GraphQLObjectType({
     },
 
     // Delete a client
+    deleteClient: {
+      type: ClientType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        Project.find({ clientId: args.id }).then((projects) => {
+          projects.forEach((project) => {
+            project.deleteOne();
+          });
+        });
+
+        return Client.findByIdAndRemove(args.id);
+      },
+    },
+
+    // Add a project
     addProject: {
       type: ProjectType,
       args: {
