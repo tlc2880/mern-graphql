9e4954abb9b999675ae84f308f6cccc4755789e2
tlc2880 <tommylcao@gmail.com>
2023-08-15 06:06:38 -0700

    Add client deleteClient

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import Header from "./components/Header";
+import Clients from "./components/Clients";
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+// This code is used to remove a warning from the console
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache,//: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Clients />
+        </div>
+      </ApolloProvider>
+    </>
+  );
+}
+
+export default App;
