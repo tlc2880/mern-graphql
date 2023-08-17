a64299b0465483b9c0ac19bce1df11cd0960e42c
tlc2880 <tommylcao@gmail.com>
2023-08-16 18:45:04 -0700

    Add react modal dialog addClient

--- client/src/mutations/clientMutations.js
+++ client/src/mutations/clientMutations.js
@@ -1,4 +1,15 @@
 import { gql } from '@apollo/client';
+const ADD_CLIENT = gql`
+    mutation addClient($name: String!, $email: String!, $phone: String!) {
+        addClient(name: $name, email: $email, phone: $phone)
+        {
+            id
+            name
+            email
+            phone
+        }
+    }
+`;
 
 const DELETE_CLIENT = gql`
   mutation deleteClient($id: ID!) {
@@ -11,4 +22,4 @@ const DELETE_CLIENT = gql`
   }
 `;
 
-export { DELETE_CLIENT };
+export { DELETE_CLIENT, ADD_CLIENT };
